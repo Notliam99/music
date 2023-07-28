62eb8759f23f95024189d0dbe02b2b1597a7bf07
Liam Tietjens <l.tietjens@hotmail.com>
2023-07-29 00:03:57 +1200

    grabs keys from users spotdl configs last commit for the day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from spotdl import Spotdl
-from spotdl.utils.config import DOWNLOADER_OPTIONS
+from spotdl.utils.config import DOWNLOADER_OPTIONS, get_config
 
 download_options = DOWNLOADER_OPTIONS.copy()
 download_options["log_level"] = "debug"
@@ -8,8 +8,14 @@ download_options['format'] = "flac"
 download_options['threads'] = 8
 
 spotdl = Spotdl(
-        client_id='5f573c9620494bae87890c0f08a60293', 
-        client_secret='212476d9b0f3472eaa762d90b19b0ba8', 
+        client_id=get_config()["client_id"], 
+        client_secret=get_config()["client_secret"], 
         user_auth=False, 
-        downloader_settings=download_options
+        downloader_settings=download_options,
         )
+
+songs = spotdl.search(['joji - test drive',
+    'https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT'])
+
+results = spotdl.download_songs(songs)
+song, path = spotdl.download(songs[0])
